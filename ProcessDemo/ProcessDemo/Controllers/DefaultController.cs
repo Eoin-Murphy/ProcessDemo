12e6bf4c0807f7373f09dfb40ab4d9b4e5b6fea9
Eoin Murphy <eoin@henkelmurphy.com>
2015-08-31 21:04:20 +0200

    adding viewmodels, basic view and dummy data generator

--- ProcessDemo/ProcessDemo/Controllers/DefaultController.cs
+++ ProcessDemo/ProcessDemo/Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProcessDemo.Models;
 
 namespace ProcessDemo.Controllers
 {
@@ -11,7 +12,37 @@ namespace ProcessDemo.Controllers
         // GET: Default
         public ActionResult Index()
         {
-            return View();
+            var viewData = BuildDummyData(10, 10);
+
+            return View(viewData);
+        }
+
+        private List<ProcessDataViewModel> BuildDummyData(int processCount, int processItemCount )
+        {
+            var result = new List<ProcessDataViewModel>();
+            for(int i =0; i <= processCount; i++)
+            {
+                var processData = new ProcessDataViewModel
+                {
+                    Id = i.ToString(),
+                    Name = "Process " + i,
+                    Status = i % 2 == 0 ? "Completed" : "Queued"
+                };
+
+                for(int j = 0; j < processItemCount; j++)
+                {
+                    var processDataItem = new ProcessDataItemViewModel
+                    {
+                        Status = j % 2 == 0 ? "Completed" : "Queued"
+                    };
+
+                    processData.DataItems.Add(processDataItem);
+                }
+
+                result.Add(processData);
+            }
+
+            return result;
         }
     }
 }
---
+++ ProcessDemo/ProcessDemo/Models/ProcessDataViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProcessDemo.Models
+{
+    public class ProcessDataViewModel
+    {
+        public ProcessDataViewModel()
+        {
+            DataItems = new List<ProcessDataItemViewModel>();
+        }
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public List<ProcessDataItemViewModel> DataItems { get; set; }
+    }
+
+    public class ProcessDataItemViewModel
+    {
+        public string Status { get; set; }
+    }
+}
